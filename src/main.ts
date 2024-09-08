fc70f357e1f20b94450c27495c88ca7be90fad7d
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-09-07 23:54:47 -0600

    :recycle: Update animation links as needed

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,20 @@ const deck = new Reveal({
   slideNumber: true,
 });
 deck.initialize();
+
+(function prependBase() {
+  const base = import.meta.env.BASE_URL;
+  if (!base) {
+    return;
+  }
+  document.querySelectorAll('motion-canvas-player').forEach(player => {
+    let url = player.getAttribute('src');
+    if (url?.startsWith('/')) {
+      url = base + url.slice(1);
+      const newElement = document.createElement('motion-canvas-player');
+      newElement.setAttribute('auto', player.getAttribute('auto') ?? 'true');
+      newElement.setAttribute('src', url);
+      player.replaceWith(newElement);
+    }
+  });
+})();
